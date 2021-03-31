9e29dfd44b4832e9be5de236a394e9fef9ee6ba3
seedhartha <seedhartha.github@gmail.com>
2021-03-31 15:02:20 +0700

    Implement ASCII PTH file import

--- nvb/nvb_ops_path.py
+++ nvb/nvb_ops_path.py
@@ -1,6 +1,7 @@
 import bpy
 import bpy_extras
 
+from . import nvb_def
 from . import nvb_utils
 
 
@@ -27,6 +28,45 @@ class KB_OT_remove_connection(bpy.types.Operator):
 
     def execute(self, context):
         context.object.nvb.path_connections.remove(context.object.nvb.active_path_connection)
+        return {'FINISHED'}
+
+
+class KB_OT_import_path(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
+    '''Import Odyssey Engine path (.pth)'''
+
+    bl_idname = 'kb.pthimport'
+    bl_label  = 'Import Odyssey PTH'
+
+    filename_ext = '.pth'
+
+    filter_glob : bpy.props.StringProperty(
+            default = '*.pth',
+            options = {'HIDDEN'})
+
+    def execute(self, context):
+        lines = [line.strip().split() for line in open(self.filepath, 'r')]
+
+        # First pass: read points, create point objects
+        for line in lines:
+            if len(line) == 5:
+                an_object = bpy.data.objects.new(line[0], None)
+                an_object.location = [float(x) for x in line[1:4]]
+                an_object.nvb.dummytype = nvb_def.Dummytype.PATHPOINT
+                bpy.context.collection.objects.link(an_object)
+
+        # Second pass: read connections, append to point objects
+        point = None
+        for line in lines:
+            if len(line) == 5: # point
+                name = line[0]
+                if name in bpy.data.objects:
+                    point = bpy.data.objects[name]
+            elif len(line) == 1: # connection
+                name = line[0]
+                if name in bpy.data.objects:
+                    conn = point.nvb.path_connections.add()
+                    conn.point = name
+
         return {'FINISHED'}
 
 
