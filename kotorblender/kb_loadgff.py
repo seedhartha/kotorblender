f2850ecfed5decb6ef8d785c82ce1a8797cedcc9
seedhartha <seedhartha.github@gmail.com>
2021-10-16 12:26:34 +0700

    Initial binary GFF handling code

---
+++ kotorblender/kb_loadgff.py
@@ -0,0 +1,9 @@
+from kotorblender.kb_readbin import BinaryReader
+
+class GffLoader:
+
+    def __init__(self, path):
+        self.reader = BinaryReader(path, 'little')
+
+    def save():
+        pass
