b977eb31074c04d1f610414f8bf983b4a67ed95b
seedhartha <seedhartha.github@gmail.com>
2021-10-23 18:37:06 +0700

    Add animations to animation list

--- kotorblender/scene/animation.py
+++ kotorblender/scene/animation.py
@@ -16,9 +16,38 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+from .. import defines, utils
+
+
 class Animation:
-    def __init__(self, name, length, root_node, events):
+
+    def __init__(self, name, length, transtime, animroot, root_node, events):
         self.name = name
         self.length = length
+        self.transtime = transtime
+        self.animroot = animroot
         self.root_node = root_node
         self.events = events
+
+    def add_to_object(self, obj):
+        list_item = self.add_to_anim_list(obj)
+        self.add_events_to_list_item(list_item)
+
+    def add_to_anim_list(self, obj):
+        item = utils.create_anim_list_item(obj)
+        item.name = self.name
+        item.transtime = defines.fps * self.transtime
+        item.root = item.root_obj = self.get_anim_target(obj).name
+        item.frameEnd = utils.nwtime2frame(self.length) + item.frameStart
+        return item
+
+    def add_events_to_list_item(self, item):
+        for time, name in self.events:
+            event = item.eventList.add()
+            event.name = name
+            event.frame = utils.nwtime2frame(time) + item.frameStart
+
+    def get_anim_target(self, obj):
+        return utils.search_node(
+            obj,
+            lambda o, name=self.animroot: o.name.lower() == name.lower())
