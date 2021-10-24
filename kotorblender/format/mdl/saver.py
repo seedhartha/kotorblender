d13857f4d220fea271d237b50b1ede172e4b92bf
seedhartha <seedhartha.github@gmail.com>
2021-10-24 22:44:54 +0700

    Load light flare properties from binary MDL

--- kotorblender/format/mdl/saver.py
+++ kotorblender/format/mdl/saver.py
@@ -18,23 +18,11 @@
 
 from ..binwriter import BinaryWriter
 
+
 class MdlSaver:
 
     def __init__(self, path):
         self.mdl = BinaryWriter(path, 'little')
 
     def save(self):
-        self.save_file_header()
-        self.save_geometry_header()
-        self.save_model_header()
-
-    def save_file_header(self):
-        self.mdl.put_uint32(0) # signature
-        self.mdl.put_uint32(0) # MDL size
-        self.mdl.put_uint32(0) # MDX size
-
-    def save_geometry_header(self):
-        pass
-
-    def save_model_header(self):
-        pass
+        raise NotImplementedError()
