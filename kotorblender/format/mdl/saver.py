d50988354c56272223db3398611c249459789b08
seedhartha <seedhartha.github@gmail.com>
2021-10-31 19:53:13 +0700

    Implement exporting controllers to binary MDL

--- kotorblender/format/mdl/saver.py
+++ kotorblender/format/mdl/saver.py
@@ -40,13 +40,20 @@ class MdlSaver:
         self.mdx_size = 0
         self.off_name_offsets = 0
 
-        self.nodes_df = []  # depth first array of nodes
-        self.name_offsets_df = []  # depth first array of name offsets
-        self.node_offsets_df = []  # depth first array of node offsets
-        self.node_children_offsets_df = []  # depth first array of offsets to node children array
+        self.nodes = []
+        self.name_offsets = []
+        self.node_offsets = []
+        self.children_offsets = []
 
         self.parent_indices = []
         self.child_indices = []
+
+        self.controller_keys = []
+        self.controller_data = []
+        self.controller_offsets = []
+        self.controller_counts = []
+        self.controller_data_offsets = []
+        self.controller_data_counts = []
 
     def save(self):
         self.peek_model()
@@ -63,13 +70,65 @@ class MdlSaver:
 
         self.peek_nodes(self.model.root_node)
 
-        self.mdl_pos += 4 * len(self.nodes_df)  # name offsets
+        self.mdl_pos += 4 * len(self.nodes)  # name offsets
 
         self.peek_node_names()
         self.peek_node_data()
 
         self.mdl_size = self.mdl_pos
         self.mdx_size = self.mdx_pos
+
+    def peek_nodes(self, node, parent_idx=None):
+        node_idx = len(self.nodes)
+        self.nodes.append(node)
+        self.parent_indices.append(parent_idx)
+        self.child_indices.append([])
+
+        for child in node.children:
+            child_idx = len(self.nodes)
+            self.child_indices[node_idx].append(child_idx)
+            self.peek_nodes(child, node_idx)
+
+    def peek_node_names(self):
+        for node in self.nodes:
+            self.name_offsets.append(self.mdl_pos)
+            self.mdl_pos += len(node.name) + 1
+
+    def peek_node_data(self):
+        for node in self.nodes:
+            self.node_offsets.append(self.mdl_pos)
+            self.mdl_pos += 80  # geometry header
+
+            ctrl_keys = []
+            ctrl_data = []
+            self.peek_controllers(node, ctrl_keys, ctrl_data)
+            ctrl_count = len(ctrl_keys)
+            ctrl_data_count = len(ctrl_data)
+            self.controller_keys.append(ctrl_keys)
+            self.controller_data.append(ctrl_data)
+            self.controller_counts.append(ctrl_count)
+            self.controller_data_counts.append(ctrl_data_count)
+
+            self.controller_offsets.append(self.mdl_pos)
+            self.mdl_pos += 16 * ctrl_count
+
+            self.controller_data_offsets.append(self.mdl_pos)
+            self.mdl_pos += 4 * ctrl_data_count
+
+            self.children_offsets.append(self.mdl_pos)
+            self.mdl_pos += 4 * len(node.children)
+
+    def peek_controllers(self, node, out_keys, out_data):
+        if node.parent:
+            return
+        out_keys.append(ControllerKey(CTRL_BASE_POSITION, 1, 0, 1, 3))
+        out_data.append(0.0)  # timekey
+        for val in node.position:
+            out_data.append(val)
+        out_keys.append(ControllerKey(CTRL_BASE_ORIENTATION, 1, 4, 5, 4))
+        out_data.append(0.0)  # timekey
+        for val in node.orientation:
+            out_data.append(val)
 
     def save_file_header(self):
         self.mdl.put_uint32(0)  # pseudo signature
@@ -84,8 +143,8 @@ class MdlSaver:
             fn_ptr1 = MODEL_FN_PTR_1_K1_PC
             fn_ptr2 = MODEL_FN_PTR_2_K1_PC
         model_name = self.model.name.ljust(32, '\0')
-        off_root_node = self.node_offsets_df[0]
-        total_num_nodes = len(self.nodes_df)
+        off_root_node = self.node_offsets[0]
+        total_num_nodes = len(self.nodes)
         ref_count = 0
         model_type = 2
 
@@ -111,7 +170,7 @@ class MdlSaver:
         radius = 7.0  # TODO
         scale = self.model.animscale
         supermodel_name = self.model.supermodel.ljust(32, '\0')
-        off_head_root_node = self.node_offsets_df[0]
+        off_head_root_node = self.node_offsets[0]
         mdx_size = self.mdx_size
         mdx_offset = 0
 
@@ -131,16 +190,16 @@ class MdlSaver:
         self.mdl.put_uint32(0)  # unknown
         self.mdl.put_uint32(mdx_size)
         self.mdl.put_uint32(mdx_offset)
-        self.put_array_def(self.off_name_offsets, len(self.nodes_df))  # name offsets
+        self.put_array_def(self.off_name_offsets, len(self.nodes))  # name offsets
 
     def save_names(self):
-        for offset in self.name_offsets_df:
+        for offset in self.name_offsets:
             self.mdl.put_uint32(offset)
-        for node in self.nodes_df:
+        for node in self.nodes:
             self.mdl.put_c_string(node.name)
 
     def save_nodes(self):
-        for node_idx, node in enumerate(self.nodes_df):
+        for node_idx, node in enumerate(self.nodes):
             print("Persisting {} {}".format(node_idx, vars(node)))
 
             type_flags = NODE_BASE  # TODO
@@ -148,7 +207,7 @@ class MdlSaver:
             name_index = node_idx
             off_root = 0
             parent_idx = self.parent_indices[node_idx]
-            off_parent = self.node_offsets_df[parent_idx] if parent_idx else 0
+            off_parent = self.node_offsets[parent_idx] if parent_idx else 0
             position = node.position
             orientation = node.orientation
             child_indices = self.child_indices[node_idx]
@@ -163,37 +222,30 @@ class MdlSaver:
                 self.mdl.put_float(val)
             for val in orientation:
                 self.mdl.put_float(val)
-            self.put_array_def(self.node_children_offsets_df[node_idx], len(child_indices))  # children
-            self.put_array_def(0, 0)  # controllers
-            self.put_array_def(0, 0)  # controller data
+            self.put_array_def(self.children_offsets[node_idx], len(child_indices))
+            self.put_array_def(self.controller_offsets[node_idx], self.controller_counts[node_idx])
+            self.put_array_def(self.controller_data_offsets[node_idx], self.controller_data_counts[node_idx])
 
             # TODO: support all node types
 
+            for key in self.controller_keys[node_idx]:
+                unk1 = 0xffff
+
+                self.mdl.put_uint32(key.ctrl_type)
+                self.mdl.put_uint16(unk1)
+                self.mdl.put_uint16(key.num_rows)
+                self.mdl.put_uint16(key.timekeys_start)
+                self.mdl.put_uint16(key.values_start)
+                self.mdl.put_uint8(key.num_columns)
+
+                for _ in range(3):
+                    self.mdl.put_uint8(0)  # padding
+
+            for val in self.controller_data[node_idx]:
+                self.mdl.put_float(val)
+
             for child_idx in child_indices:
-                self.mdl.put_uint32(self.node_offsets_df[child_idx])
-
-    def peek_nodes(self, node, parent_idx=None):
-        node_idx = len(self.nodes_df)
-        self.nodes_df.append(node)
-        self.parent_indices.append(parent_idx)
-        self.child_indices.append([])
-
-        for child in node.children:
-            child_idx = len(self.nodes_df)
-            self.child_indices[node_idx].append(child_idx)
-            self.peek_nodes(child, node_idx)
-
-    def peek_node_names(self):
-        for node in self.nodes_df:
-            self.name_offsets_df.append(self.mdl_pos)
-            self.mdl_pos += len(node.name) + 1
-
-    def peek_node_data(self):
-        for node in self.nodes_df:
-            self.node_offsets_df.append(self.mdl_pos)
-            self.mdl_pos += 80  # geometry header
-            self.node_children_offsets_df.append(self.mdl_pos)
-            self.mdl_pos += 4 * len(node.children)  # child offsets
+                self.mdl.put_uint32(self.node_offsets[child_idx])
 
     def put_array_def(self, offset, count):
         self.mdl.put_uint32(offset)
