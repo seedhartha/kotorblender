73985e186284f409fa1d4d4d19c1233aa4c75195
seedhartha <seedhartha.github@gmail.com>
2023-08-12 09:57:38 +0100

    Fallback to evenly sized lists for degenerate AABB trees

--- kotorblender/aabb.py
+++ kotorblender/aabb.py
@@ -21,84 +21,114 @@ from mathutils import Vector
 from . import utils
 
 
-def generate_tree(aabb_tree, face_list, rlevel=0):
-    if rlevel > 128:
-        raise ValueError("rlevel must not exceed 128, but is equal to {}".format(rlevel))
+class BoundingBox:
+    def __init__(self, min, max, center):
+        self.min = min
+        self.max = max
+        self.center = center
 
-    if not face_list:
-        raise ValueError("face_list must not be empty")
+    def longest_axis(self):
+        size = self.max - self.min
+        if size.y > size.x and size.y > size.z:
+            return 1  # Y
+        elif size.z > size.x and size.z > size.y:
+            return 2  # Z
+        else:
+            return 0  # X
 
-    # Calculate bounding box min/max and centroid
-    bb_min = Vector((100000.0, 100000.0, 100000.0))
-    bb_max = Vector((-100000.0, -100000.0, -100000.0))
-    bb_centroid = Vector()
-    for face in face_list:
-        face_verts = face[1]
-        for vert in face_verts:
-            for axis in range(3):
-                if bb_min[axis] > vert[axis]:
-                    bb_min[axis] = vert[axis]
-                if bb_max[axis] < vert[axis]:
-                    bb_max[axis] = vert[axis]
-        face_centroid = face[2]
-        bb_centroid = bb_centroid + face_centroid
-    bb_centroid = bb_centroid / len(face_list)
+
+def generate_tree(aabb_tree, faces, depth=0):
+    if depth > 128:
+        raise ValueError("depth must not exceed 128, but is equal to {}".format(depth))
+    if not faces:
+        raise ValueError("faces must not be empty")
+
+    bounding_box = compute_bounding_box(faces)
 
     # Only one face left - this node is a leaf
-    if len(face_list) == 1:
-        face_idx = face_list[0][0]
-        aabb_tree.append([*bb_min[:3], *bb_max[:3], -1, -1, face_idx, 0])
+    if len(faces) == 1:
+        face_idx = faces[0][0]
+        aabb_tree.append(new_aabb_node(bounding_box, -1, -1, face_idx, 0))
         return
 
-    # Find longest axis
-    bb_size = bb_max - bb_min
-    if bb_size.y > bb_size.x and bb_size.y > bb_size.z:
-        split_axis = 1
-    elif bb_size.z > bb_size.x and bb_size.z > bb_size.y:
-        split_axis = 2
-    else:
-        split_axis = 0
+    split_axis = find_split_axis(bounding_box, faces)
+    left_faces, right_faces, actual_split_axis = split_faces(bounding_box, faces, split_axis)
 
-    # Change axis in case points are coplanar with the split plane
-    change_axis = True
-    for face in face_list:
-        face_centroid = face[2]
-        change_axis = change_axis and utils.is_close(face_centroid[split_axis], bb_centroid[split_axis], 1e-4)
-    if change_axis:
-        split_axis += 1
-        if split_axis == 3:
-            split_axis = 0
-
-    # Put faces on the left and right side of the split plane into separate
-    # lists. Try all axises to prevent tree degeneration.
-    face_list_left = []
-    face_list_right = []
-    for i in range(0, 4):
-        if i == 3:
-            raise RuntimeError("Generated tree is degenerate")
-
-        # Sort faces by side
-        face_list_left = []
-        face_list_right = []
-        for face in face_list:
-            face_centroid = face[2]
-            if face_centroid[split_axis] < bb_centroid[split_axis]:
-                face_list_left.append(face)
-            else:
-                face_list_right.append(face)
-
-        # Neither list is empty, this split will do just fine
-        if face_list_left and face_list_right:
-            break
-
-        # At least one of the lists is empty - try another axis
-        split_axis += 1
-        if split_axis == 3:
-            split_axis = 0
-
-    node = [*bb_min[:3], *bb_max[:3], 0, 0, -1, 1 + split_axis]
+    node = new_aabb_node(bounding_box, 0, 0, -1, 1 + actual_split_axis)
     aabb_tree.append(node)
     node[6] = len(aabb_tree)
-    generate_tree(aabb_tree, face_list_left, rlevel+1)
+    generate_tree(aabb_tree, left_faces, depth + 1)
     node[7] = len(aabb_tree)
-    generate_tree(aabb_tree, face_list_right, rlevel+1)
+    generate_tree(aabb_tree, right_faces, depth + 1)
+
+
+def compute_bounding_box(faces):
+    min = Vector((100000.0, 100000.0, 100000.0))
+    max = Vector((-100000.0, -100000.0, -100000.0))
+    center = Vector()
+    for face in faces:
+        verts = face[1]
+        for vert in verts:
+            for axis in range(3):
+                if min[axis] > vert[axis]:
+                    min[axis] = vert[axis]
+                if max[axis] < vert[axis]:
+                    max[axis] = vert[axis]
+        face_center = face[2]
+        center = center + face_center
+    center = center / len(faces)
+    return BoundingBox(min, max, center)
+
+
+def find_split_axis(bounding_box, faces):
+    axis = bounding_box.longest_axis()
+
+    # Change axis in case points are coplanar with the split plane
+    change = True
+    for face in faces:
+        face_center = face[2]
+        change = change and utils.is_close(face_center[axis], bounding_box.center[axis], 1e-4)
+    if change:
+        axis += 1
+        if axis == 3:
+            axis = 0
+
+    return axis
+
+
+def split_faces(bounding_box, faces, split_axis):
+    # Put faces on the left and right side of the split plane into separate
+    # lists. Try all axises to prevent tree degeneration.
+    left_faces = []
+    right_faces = []
+    for i in range(4):
+        left_faces = []
+        right_faces = []
+        for face in faces:
+            face_center = face[2]
+            if face_center[split_axis] < bounding_box.center[split_axis]:
+                left_faces.append(face)
+            else:
+                right_faces.append(face)
+        if left_faces and right_faces:
+            break
+        # Tree is degenerate. Try another axis, or split into evenly sized
+        # lists if already tried all 3.
+        if i == 3:
+            non_empty_faces = left_faces if left_faces else right_faces
+            empty_faces = left_faces if not left_faces else right_faces
+            num_faces_to_move = int(len(non_empty_faces) / 2)
+            for _ in range(num_faces_to_move):
+                empty_faces.append(non_empty_faces.pop())
+        split_axis = (split_axis + 1) % 3
+    return (left_faces, right_faces, split_axis)
+
+
+def new_aabb_node(bounding_box, left_child_idx, right_child_idx, face_idx, most_significant_plane):
+    return [
+        *bounding_box.min[:3],
+        *bounding_box.max[:3],
+        left_child_idx,
+        right_child_idx,
+        face_idx,
+        most_significant_plane]
